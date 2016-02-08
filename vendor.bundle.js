59fd66625157cfb383eeccb21080f836007d9431
Dumplingpuff <jason.liang2012@gmail.com>
2016-02-07 22:11:15 -0500

    Made changes to bundle.js and vendor.bundle.js

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(19);
+	module.exports = __webpack_require__(17);
 
 
 /***/ },
@@ -9952,7 +9952,7 @@
 
 /***/ },
 
-/***/ 19:
+/***/ 17:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
